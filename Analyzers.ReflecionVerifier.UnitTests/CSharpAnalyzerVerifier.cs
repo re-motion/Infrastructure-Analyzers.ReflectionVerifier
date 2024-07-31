a262b1e106e1760bac561a02b8cbacc621c35389
Michael Ludwig <michael.ludwig1392@gmail.com>
2024-07-31 10:35:04 +0200

    added method to make Assembly referencing easy

--- Analyzers.ReflecionVerifier.UnitTests/CSharpAnalyzerVerifier.cs
+++ Analyzers.ReflecionVerifier.UnitTests/CSharpAnalyzerVerifier.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 using System;
+using System.Reflection;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Testing;
@@ -20,16 +22,18 @@ public static class CSharpAnalyzerVerifier<TAnalyzer>
 
   public static Task VerifyAnalyzerAsync(string source, params DiagnosticResult[] expected)
   {
+    //var contextAssemblyLocation = typeof(...).Assembly.Location;
 
     var test = new Test
     {
       TestCode = source,
-
+      //ReferenceAssemblies = GetReferenceAssemblies(typeof(...).Assembly),
       SolutionTransforms =
       {
         (solution, id) =>
         {
           var project = solution.GetProject(id)!;
+          //project = project.AddMetadataReference(MetadataReference.CreateFromFile(contextAssemblyLocation));
           return project.Solution;
         }
       }
@@ -37,6 +41,16 @@ public static class CSharpAnalyzerVerifier<TAnalyzer>
     test.ExpectedDiagnostics.AddRange(expected);
 
     return test.RunAsync();
+  }
+
+  private static ReferenceAssemblies GetReferenceAssemblies(Assembly assembly)
+  {
+    return assembly.GetCustomAttribute<TargetFrameworkAttribute>()!.FrameworkName switch
+    {
+      ".NETCoreApp,Version=v8.0" => ReferenceAssemblies.Net.Net80,
+      ".NETStandard,Version=v2.0" => ReferenceAssemblies.NetStandard.NetStandard20,
+      var frameworkName => throw new NotSupportedException($"'{frameworkName}' is not supported.")
+    };
   }
 
   private class Test : CSharpAnalyzerTest<TAnalyzer, DefaultVerifier>;
